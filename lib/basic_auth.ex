afd0c9737dd4247a25b29352d4d2569f28d01879
Mark Connell <mark@markconnell.co.uk>
2015-08-10 11:19:19 +0100

    force plug to return 401 with appropriate header

--- lib/basic_auth.ex
+++ lib/basic_auth.ex
@@ -1,7 +1,12 @@
 defmodule BasicAuth do
-  def init(_options) do
+
+  def init(options) do
+    options
   end
 
-  def call(_conn, _options) do
+  def call(conn, _options) do
+    Plug.Conn.put_resp_header(conn, "www-authenticate", "Basic realm=\"WallyWorld\"")
+    |> Plug.Conn.send_resp(401, "401 Unauthorized")
+    |> Plug.Conn.halt
   end
 end
