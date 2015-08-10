4200a00b1dfea180c1bf3225276ca2b8ed5a6597
Mark Connell <mark@markconnell.co.uk>
2015-08-10 13:47:28 +0100

    dependencies

--- mix.exs
+++ mix.exs
@@ -10,23 +10,24 @@ defmodule BasicAuth.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :cowboy, :plug]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [{:cowboy, "~> 1.0"},
+     {:plug, "~> 0.14 or ~> 1.0"}]
   end
+
+  defp package do
+    [contributors: ["Mark Connell",],
+     licenses: ["MIT"],
+     links: %{github: "https://github.com/mconnell/basic_auth"},
+     files: ~w(lib LICENSE.md mix.exs README.md)]
+  end
+
+  defp description do
+    "Basic Authentication Plug"
+  end
+
 end
