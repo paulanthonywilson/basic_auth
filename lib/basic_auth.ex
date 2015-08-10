acb58cd9606a88b62e19597b1f65b60faabd61ae
Mark Connell <mark@markconnell.co.uk>
2015-08-10 11:31:25 +0100

    break unauthorized out. start faking out valid credentials

--- lib/basic_auth.ex
+++ lib/basic_auth.ex
@@ -5,6 +5,22 @@ defmodule BasicAuth do
   end
 
   def call(conn, _options) do
+    credentials = Plug.Conn.get_req_header(conn, "authorization")
+
+    if credentials |> valid_credentials? do
+      conn
+    else
+      conn
+      |> send_unauthorized_response
+    end
+  end
+
+  defp valid_credentials?(credentials) do
+    # true
+    false
+  end
+
+  defp send_unauthorized_response(conn) do
     Plug.Conn.put_resp_header(conn, "www-authenticate", "Basic realm=\"WallyWorld\"")
     |> Plug.Conn.send_resp(401, "401 Unauthorized")
     |> Plug.Conn.halt
