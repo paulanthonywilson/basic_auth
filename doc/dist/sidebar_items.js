6b4885b16aaae0147a92fe8efce19f1ae3641f97
Paul Wilson <paulanthonywilson@gmail.com>
2017-04-07 15:06:18 +0100

    ignore doc

--- doc/dist/sidebar_items.js
+++
@@ -1,1 +0,0 @@
-sidebarNodes={"exceptions":[],"extras":[{"id":"api-reference","title":"API Reference","group":"","headers":[{"id":"Modules","anchor":"modules"}]}],"modules":[{"id":"BasicAuth","title":"BasicAuth","functions":[{"id":"call/2","anchor":"call/2"},{"id":"init/1","anchor":"init/1"}]}],"protocols":[]}
