e0a16f3ae53880e44b9e553bf84bb434ca297025
Paul Wilson <paulanthonywilson@gmail.com>
2017-06-28 14:24:23 +0100

    bump version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule BasicAuth.Mixfile do
     [app: :basic_auth,
      description: "Basic Authentication Plug",
      package: package(),
-     version: "2.1.2",
+     version: "2.1.3",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
