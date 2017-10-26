1de1a29ec558cf55f99d1301cef0996912a9994d
Scott S <scottswezey@users.noreply.github.com>
2017-10-26 03:05:39 -0700

    Add content type to 401 page response headers (#31)
    This should avoid strange behavior in Safari. With this change, Safari should display the error message. Without the change, it downloads the error page as a file.

--- lib/basic_auth/response.ex
+++ lib/basic_auth/response.ex
@@ -8,6 +8,7 @@ defmodule BasicAuth.Response do
   def unauthorise(conn, realm) do
     conn
     |> Plug.Conn.put_resp_header("www-authenticate", "Basic realm=\"#{realm || @default_realm}\"")
+    |> Plug.Conn.put_resp_content_type("text/plain")
     |> Plug.Conn.send_resp(401, "401 Unauthorized")
   end
 end
